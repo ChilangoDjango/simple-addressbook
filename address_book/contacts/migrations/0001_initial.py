594406cdb652d38345792afd47ddb8c5710c3461
gmora08 <mnr.hh08@gmail.com>
2015-06-06 05:12:48 -0500

    set models

---
+++ address_book/contacts/migrations/0001_initial.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Contact',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+                ('last_name', models.CharField(max_length=255, null=True, blank=True)),
+                ('address', models.CharField(max_length=255, null=True, blank=True)),
+                ('email', models.EmailField(max_length=254, null=True, blank=True)),
+                ('type_number', models.CharField(max_length=255)),
+                ('number', models.IntegerField()),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Group',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+                ('description', models.CharField(max_length=255)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
--- address_book/contacts/models.py
+++ address_book/contacts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    type_number = models.CharField(max_length=255)
+    number = models.IntegerField()
+    user = models.ForeignKey(User)
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    user = models.ForeignKey(User)
