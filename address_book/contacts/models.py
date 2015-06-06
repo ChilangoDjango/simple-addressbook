d03fe1277cc04b17edcd64a39336be4ebe00df99
gmora08 <mnr.hh08@gmail.com>
2015-06-06 05:37:13 -0500

    change model

--- address_book/contacts/models.py
+++ address_book/contacts/models.py
@@ -1,18 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
+from . import choices
 
 class Contact(models.Model):
-    name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255, blank=True, null=True)
-    address = models.CharField(max_length=255, blank=True, null=True)
-    email = models.EmailField(blank=True, null=True)
-    type_number = models.CharField(max_length=255)
-    number = models.IntegerField()
+    name = models.CharField('Nombre', max_length=255)
+    last_name = models.CharField('Apellidos', max_length=255, blank=True, null=True)
+    address = models.CharField('Direccion', max_length=255, blank=True, null=True)
+    email = models.EmailField('Correo', blank=True, null=True)
+    type_number = models.IntegerField('Tipo', choices=choices.type_number)
+    number = models.IntegerField('Numero')
     user = models.ForeignKey(User)
+
+    def __unicode__(self): #__str__ python 3
+        return self.name + ' ' + self.last_name
 
 
 class Group(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
+    name = models.CharField('Nombre', max_length=255)
+    description = models.CharField('Descripcion', max_length=255)
     user = models.ForeignKey(User)
-    contact = models.ManyToManyField(Contact)
+    contact = models.ManyToManyField(Contact, verbose_name="Contactos")
+
+    def __unicode__(self): #__str__ python 3
+        return self.name
